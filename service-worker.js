e359d9c81f7c3a134ac8d1080b1045afbbca6428
Aniqzz <wananiqkk110@gmail.com>
2025-03-11 17:35:31 +0800

    pwas fix 3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = ["/docs/index.html", "docs/manifest.json"];
+const urlsToCache = ["/docs/index.html", "manifest.json"];
 
 self.addEventListener("install", event => {
     event.waitUntil(
